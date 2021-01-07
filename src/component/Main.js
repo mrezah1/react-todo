eddece6e949925961bb2cb503418bad38d6e68a6
Mreza <Mrhhajiyan@gmail.com>
2021-01-08 01:32:24 +0330

    TodoList version2 => Functional Component

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,49 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FromAdd from './FormAdd/FromAdd'
 import Todo from './Todo/Todo'
 import './Main.css'
 
-class Main extends React.Component {
-    state = {
+const Main = (props) => {
+    const [todoSt, setTodo] = useState({
         todos: [],
-        inputVal: '',
-    }
-    addTodo = (e) => {
+        inputVal: ''
+    })
+    const addTodo = (e) => {
         e.preventDefault()
-        if (this.state.inputVal.trim() !== '') {
-            const newState = [...this.state.todos];
-            newState.push(this.state.inputVal);
-            this.setState({ inputVal: '', todos: newState })
+        if (todoSt.inputVal.trim() !== '') {
+            const newState = { ...todoSt };
+            newState.todos.push(todoSt.inputVal);
+            newState.inputVal = "";
+            setTodo({ ...todoSt, ...newState });
         }
     }
-    removeTodo = (id) => {
-        const newState = [...this.state.todos];
+    const removeTodo = (id) => {
+        const newState = [...todoSt.todos];
         newState.splice(id, 1);
-        this.setState({ todos: newState })
+        setTodo({ ...todoSt, todos: newState })
     }
-    changeValHandler = (event) => {
-        this.setState({ inputVal: event.target.value })
+    const changeValHandler = (event) => {
+        // this.setState({ inputVal: event.target.value })
+        setTodo({ ...todoSt, inputVal: event.target.value })
     }
-    render() {
-        return (
-            <section className="main">
-                <div className="wrapper-form">
-                    <FromAdd
-                        addTodo={this.addTodo}
-                        input={this.state.inputVal}
-                        change={this.changeValHandler}
-                    />
-                </div>
-                <ul className="wrapper-todo">
-                    {this.state.todos.length > 0 ?
-                        this.state.todos.map((item, index) => (
-                            <Todo title={item} key={index} removeTodo={() => this.removeTodo(index)} />
-                        ))
-                        : <p style={{textAlign:'center'}}>Empty Todo!</p>}
-                </ul>
-            </section>
-        )
-    }
-}
 
+    return (
+        <section className="main">
+            <div className="wrapper-form">
+                <FromAdd
+                    addTodo={addTodo}
+                    input={todoSt.inputVal}
+                    change={changeValHandler}
+                />
+            </div>
+            <ul className="wrapper-todo">
+                {todoSt.todos.length > 0 ?
+                    todoSt.todos.map((item, index) => (
+                        <Todo title={item}
+                            key={index}
+                            removeTodo={() => removeTodo(index)}
+                        />))
+                    : <p style={{ textAlign: 'center' }}>Empty Todo!</p>}
+            </ul>
+        </section>
+    )
+}
 export default Main
